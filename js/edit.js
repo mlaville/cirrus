af086f6955dea15e10dfe8101b158cb65143147e
vava <marc.laville@polinux.net>
2015-03-01 17:12:00 +0100

    restitution du menu Imprimer dans edit

--- js/edit.js
+++ js/edit.js
@@ -14,6 +14,7 @@
  * @date   revision   marc laville  04/02/2015 : Gestion des fenetre grace au winManager
  * @date   revision   marc laville  08/02/2015 : gestion du menu par menuFactory
  * @date   revision   marc laville  10/02/2015 : Enregistrement du ficjier txt
+ * @date revision marc laville 17/02/2015 : Edition -> pdf
  *
  * A faire
  * - memoriser le focus (selection) lorsqu'il est perdu par le passage sur une autre fenetre
@@ -32,7 +33,6 @@ var app_edit = {
 	quitter : function(){
 		alert("exit");
 	},
-	//--- Chargement d'un fichier texte
 	keyWindow( ) {
 		return 
 		
@@ -75,6 +75,30 @@ var app_edit = {
 		
 //		return app_edit.sauve( frontWindow.dataset.path, frontWindow.querySelector( '.edit' ).innerHTML );
 //	},
+	imprime: function( texte, target ){
+		var doc = new jsPDF(),
+			pos = { x:'5%', y:'120px', width:'880px', height: '420px' },
+			objPdf = document.createElement('object'),
+			margin = 2.5,
+			verticalOffset = margin,
+			size = 12;
+
+		doc.setDrawColor(0, 255, 0)
+			.setLineWidth(1/72)
+			.line(margin, margin, margin, 11 - margin)
+			.line(8.5 - margin, margin, 8.5-margin, 11-margin)
+		lines = doc.setFont('Courier','')
+			.setFontSize(12)
+			.splitTextToSize(texte, 180);
+		doc.text(2.5, verticalOffset + size / 72, lines);
+		// doc.text(20, 20, texte);
+		objPdf.setAttribute('type', 'application/pdf');
+		objPdf.setAttribute('width', '100%');
+		objPdf.setAttribute('height', '100%');
+		objPdf.setAttribute('data', doc.output('datauristring'));
+		
+		return winManager.domFenetre( 'Edition', objPdf, 'pdf', pos );
+	},
 	open : function( path ){
 		var divEdit = document.createElement("div"),
 			winTxt = winManager.domFenetre( 'Edit - ' + ( path || 'Nouveau document' ), divEdit, this.appName );
@@ -86,7 +110,7 @@ var app_edit = {
 			this.load( path, divEdit )
 			winTxt.dataset.path = path;
 		} else {
-			divEdit.innerHTML = 'Hello Word !';
+//			divEdit.innerHTML = 'Hello Word !';
 			winTxt.dataset.path = 'Users/home/vava/-bureau/Nouveau document.txt';
 		}
 		
@@ -103,21 +127,24 @@ var app_edit = {
 		var menuApp = menuFactory.domMenu("Edit"),
 			itemFichier = menuFactory.domItemMenu( 'Fichier', 'edit' ),
 			menuFichier = menuFactory.domMenu("Fichier"),
-			sauveFrontDoc = function( app ) {
+			impFrontDoc = function( app ) {
 				return function( e ) {
-				var frontWindow = winManager.frontWindow( app.appName );
-				
-				return app.sauve( frontWindow.dataset.path, frontWindow.querySelector( '.edit' ).innerHTML, e.target );
+					var frontWindow = winManager.frontWindow( app.appName );
+					return app.imprime( frontWindow.querySelector( '.edit' ).innerHTML.replace( /<br>/g, '\n' ), e.target );
 				}
 			};
-			
+			sauveFrontDoc = function( app ) {
+				return function( e ) {
+					var frontWindow = winManager.frontWindow( app.appName );
+					return app.sauve( frontWindow.dataset.path, frontWindow.querySelector( '.edit' ).innerHTML, e.target );
+				}
+			};
 		menuFactory.addItem( menuApp, itemFichier );
+		menuFactory.addItem( menuApp, menuFactory.domItemMenu( 'Imprimer', 'edit', impFrontDoc(this) ) );
 		menuFactory.addItem( menuApp, menuFactory.domItemMenu( 'Quitter', 'edit', this.quit ) );
-		
 		menuFactory.addItem( menuFichier, menuFactory.domItemMenu( 'Nouveau', 'fichier', this.nouveauDoc ) );
-		menuFactory.addItem( menuFichier, menuFactory.domItemMenu( 'Sauver', 'fichier', sauveFrontDoc( this) ) );
-		menuFactory.addSubMenu(	menuFichier,  itemFichier);
-
+		menuFactory.addItem( menuFichier, menuFactory.domItemMenu( 'Sauver', 'fichier', sauveFrontDoc(this) ) );
+		menuFactory.addSubMenu( menuFichier, itemFichier);
 		return menuApp;
 	},
 	liDock : function(){
--- js/panel.js
+++ js/panel.js
@@ -13,13 +13,15 @@
  * @date   revision   marc laville  04/02/2015 Gestion de la fenetre principale ; ajout du contenaire
  * @date   revision   marc laville  08/02/2015 : menuFactory
  * @date   revision   marc laville  27/02/2015 : btnClose remplace divClose
+ * @date   revision   marc laville  01/03/2015 : revise le mécanisme de mise au premier plan
  *
  * A faire : case de miniaturisation, plein ecran
  * 
  */
 var winManager = (function (document) {
 	var contenaire = document.getElementById("workSpace") || document.body,
-//		listWindows = {},
+		listDivWindows = {},
+		rdFrontWindows = null,
 		/**
 		 * Réponse à un click sur une fenêtre
 		 * On passe le form contenant la fenêtre (this) au premier plan
@@ -29,17 +31,20 @@ var winManager = (function (document) {
 		 */
 		changeKeyWindows = function(e) {
 		
-			var forms = this.parentNode.querySelectorAll('form'),
-				lastForm = forms[forms.length - 1];
+			var rd = e.target,
+				win = rd.parentNode.parentNode,
+				listWin = listDivWindows[ rd.getAttribute('name') ],
+				lastDiv = contenaire.lastChild;
 			
-			if(lastForm !== this && lastForm.lastChild != undefined) {
-				if( lastForm.lastChild != undefined ) {
-					lastForm.lastChild.firstChild.firstChild.checked = false;
-				}
-				this.parentNode.appendChild( this.parentNode.removeChild(this) );
+			if(lastDiv !== listWin ) {
+				contenaire.appendChild( contenaire.removeChild(listWin) );
 			}
+			if( rdFrontWindows != null ) {
+				rdFrontWindows.checked = false;
+			}
+			rdFrontWindows = rd;
 			
-			return this.appendChild(this.removeChild(e.target.parentNode.parentNode));
+			return listWin.appendChild( listWin.removeChild(win) );
 		},
 		addListWindows = function( nomApp ) {
 			var formRd = contenaire.appendChild( document.createElement("form") );
@@ -47,13 +52,17 @@ var winManager = (function (document) {
 			formRd.setAttribute( 'name', nomApp );
 			formRd.addEventListener( "change", changeKeyWindows );
 			
-			return formRd;
+			listDivWindows[ nomApp ] = contenaire.appendChild( document.createElement("section") );
+			
+			return listDivWindows[ nomApp ];
 		},
 		quitApp = function( nomApp ) {
 			var formApp = document.forms[nomApp];
 			if( formApp != undefined ){
 				formApp.parentNode.removeChild(formApp);
 			}
+			
+			return contenaire.removeChild( listDivWindows[ nomApp ] );
 		},
 		/**
 		 * Ajout d'une fenêtre
@@ -64,17 +73,16 @@ var winManager = (function (document) {
 			nomApp = nomApp || "_";
 			
 			if( document.forms[nomApp] == undefined ){
-				var formRd = contenaire.appendChild( document.createElement("form") );
-				formRd.setAttribute( 'name', nomApp );
-				formRd.addEventListener( "change", changeKeyWindows );
+				addListWindows( nomApp );
 			}
 			
-			return document.forms[nomApp].appendChild(win);
+			return listDivWindows[ nomApp ].appendChild(win);
 		},
-		keyWindow = function ( nomApp ) {
-			var formApp = document.forms[nomApp];
-			
-			return (formApp == null) ? formApp.querySelector('.fenetre:last-of-type') : null;
+		/**
+		 * Retourne la fenetre au premier plan 
+		 */
+		frontWindow = function ( nomApp ) {
+			return ( listDivWindows.hasOwnProperty(nomApp) ) ? listDivWindows[ nomApp ].querySelector('.fenetre:last-of-type') : null;
 		},
 		/**
 		 * Création d'une fenêtre
@@ -85,7 +93,6 @@ var winManager = (function (document) {
 				labelRd = divFenetre.appendChild( document.createElement("label") )
 				inputRd = labelRd.appendChild( document.createElement("input") ),
 				divTitre = labelRd.appendChild( document.createElement("div") ),
-//				divClose = document.createElement("div"),
 				btnClose = document.createElement("button"),
 				divContent = labelRd.appendChild( document.createElement("div") ),
 				closeFenetre = function(e) {
@@ -97,9 +104,11 @@ var winManager = (function (document) {
 					
 					return divFenetre.parentNode.removeChild(divFenetre);
 				};
-
+			nomAppli = nomAppli || '_';
 			inputRd.setAttribute( 'type', 'radio' );
-			inputRd.setAttribute( 'name', nomAppli || '_' );
+			inputRd.setAttribute( 'name', nomAppli );
+			inputRd.setAttribute( 'form', nomAppli );
+			inputRd.addEventListener( "change", changeKeyWindows );
 			
 			if( param != undefined ) {
 				divFenetre.style.left = param.x;
@@ -109,15 +118,11 @@ var winManager = (function (document) {
 			}
 			divFenetre.className = "fenetre";
 			divTitre.className = "titreFenetre";
-//			divClose.className = "closeFenetre";
 			btnClose.className = "close";
 			divContent.className = "contenuFenetre";
-			
-//			divClose.textContent = "X";
-//			divClose.addEventListener( "click", closeFenetre );
+
 			btnClose.addEventListener( "click", closeFenetre );
 			
-//			divTitre.appendChild( divClose );
 			divTitre.appendChild( btnClose );
 			divTitre.appendChild( document.createTextNode(unTitre) );
 			if(unContenu) {
@@ -138,7 +143,7 @@ var winManager = (function (document) {
 	// listeFenetres : listDomFenetres,
 	addListWindows : addListWindows,
 	quitApp:quitApp,
-	keyWindow : keyWindow
+	frontWindow : frontWindow
   };
 }(window.document));
 
